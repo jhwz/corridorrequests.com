8e5d2ddce2d8442667f9d016ffc0cce6b46079ee
Joel <joelhowse@gmail.com>
2024-02-04 22:43:00 +1300

    try different domain

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -24,7 +24,7 @@ export const actions: Actions = {
 				}
 			],
 			subject: 'New message from corridorrequests.co.nz',
-			from: { email: companyEmail },
+			from: { email: 'no-reply@corridorrequests.co.nz' },
 			content: [
 				{
 					type: 'text/plain',
